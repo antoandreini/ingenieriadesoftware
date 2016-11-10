29e39024ccda271e31987d9f90f088157c8c70d4
Brian Flores <brianf_lp@hotmail.com>
2016-11-10 01:43:22 -0300

    validaciones

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106145028) do
+ActiveRecord::Schema.define(version: 20161110040550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(version: 20161106145028) do
     t.string   "titulo"
     t.string   "descripcion"
     t.string   "ubicacion"
-    t.boolean  "cumplida"
+    t.boolean  "cumplida",    default: false
     t.date     "fecha"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "imagen"
     t.integer  "user_id"
   end
@@ -61,26 +61,26 @@ ActiveRecord::Schema.define(version: 20161106145028) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.date     "fecha_nac"
-    t.boolean  "administrador"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "administrador",          default: false
     t.string   "nombre"
     t.string   "apellido"
-    t.integer  "puntuacion"
+    t.integer  "puntuacion",             default: 0
     t.string   "telefono"
     t.string   "ubicacion"
     t.string   "imagen"
+    t.integer  "edad"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
