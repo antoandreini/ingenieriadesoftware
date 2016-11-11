da92be39e66d87ae8e1c1b7423e01e9a6d808514
Brian Flores <brianf_lp@hotmail.com>
2016-11-11 15:38:26 -0300

    password confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nombre, :email, :password,:apellido, :edad) }
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:nombre, :email, :password, :apellido, :edad) }
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nombre, :email, :password,:password_confirmation,:apellido, :edad) }
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:nombre, :email, :password,:password_confirmation, :apellido, :edad) }
         end
 end
